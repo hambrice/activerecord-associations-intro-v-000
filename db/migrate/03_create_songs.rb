841065b0fead10e9f5ef9a284ef6ff70763b81b4
Brad Hambrice <hambrice@uab.edu>
2018-02-21 19:58:40 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,11 +1,7 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-<<<<<<< HEAD
-      t.string :name 
-=======
       t.string :name
->>>>>>> bb9abe5d044456eb9e837e660af7a8009f094a46
       t.integer :artist_id
       t.integer :genre_id
     end
